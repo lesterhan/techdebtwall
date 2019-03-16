098dc0286bd734e300d4e3d25bc1930822d6f4d4
Lester Han <lehan@thoughtworks.com>
2019-03-16 00:07:37 -0400

    Add react grid system and Quadrant component

--- techdebt-fe/src/App.tsx
+++ techdebt-fe/src/App.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+import Quadrant from './components/Quadrant';
 import './css/App.css';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <div id="tdw-text">
-          T. D. W.
-        </div>
+        <Container>
+          <Row>
+            <Col sm={6}>
+              <Quadrant />
+            </Col>
+            <Col sm={6}>
+              <Quadrant />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={6}>
+              <Quadrant />
+            </Col>
+            <Col sm={6}>
+              <Quadrant />
+            </Col>
+          </Row>
+        </Container>  
       </div>
     );
   }
---
+++ techdebt-fe/src/components/Quadrant.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import '../css/Quadrant.css'
+
+function Quadrant() {
+  return (
+    <div className='quadrant'>
+      This is a quadrant
+    </div>
+  )
+}
+
+export default Quadrant;
